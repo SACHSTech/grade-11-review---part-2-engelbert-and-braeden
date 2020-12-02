103c2ae2599ea518047250074f05166e4d599e73
Engelbert Aroozoo <engelbert.aroozoo21@ycdsbk12.ca>
2020-12-02 00:46:21 +0000

    finish arrays - 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -7,15 +7,33 @@ package gr11review.part2;
 
 import gr11review.part2.Utility.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-  
-  //Testing Methods 1
+  BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+  //Methods 1
 	String strZipZap;
 	System.out.println(Utility.zipZap("zipxxxxzopzap"));
 
-  //Testing FileIO - Read 1
+  //FileIO - Read 1
   System.out.println(Utility.longestWord("src/gr11review/part2/words.txt"));
+
+  //Array 1 - One Dimensional
+  int intInput;
+  int i;
+
+  System.out.print("How many numbers do you want to enter into the array: ");
+  intInput = Integer.parseInt(key.readLine());
+  System.out.println("Input your numbers below: ");
+  int[] nums = new int[intInput];
+    for(i = 0; i < intInput; i++) {
+     nums[i] = Integer.parseInt(key.readLine());
+    }
+    nums = Utility.tenRun(nums);
+    System.out.println(Arrays.toString(nums));
+
+  //Array 3 - One Dimensional - Two Loops
 	}
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -13,8 +13,8 @@ public class Utility {
     /**
     *Create a method that for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and *ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap"  *yields *"zpXzp".
     *
-    *@param strZipZap the string that contains the patterns with the z's and p's
-    *@return strZP the string without the letter between z and p
+    * @param strZipZap - the string that contains the patterns with the z's and p's
+    * @return strZP - the string without the letter between z and p
     * @author: E. Aroozoo
     */
 
@@ -46,8 +46,8 @@ public class Utility {
     /**
     *Write a method longestWord(String filenametxt) such that given the name of a file filenametxt that *contains a single word on each line, returns the longest word in the file.
     *
-    *@param filename the file that contains all the words
-    *@return strWord the string that is the longest in the file
+    * @param filename - the file that contains all the words
+    * @return strWord - the string that is the longest in the file
     * @author: E. Aroozoo
     */
 
@@ -72,22 +72,46 @@ public class Utility {
           if(intWordLength >= intLongestWordLength) {
             strLongestWord = strWord;
         }
-        strWord += filename.readLine();
+        strWord = filename.readLine();
       }
       filename.close();
       //Returning Longest Word
-      return strWord;
+      return strLongestWord;
     }
     /**
     *For each multiple of 10 in the given array, change all the values following it to be that multiple of *10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
     *
-    *@param filename the file that contains all the words
-    *@return strWord the string that is the longest in the file
+    * @param nums - the array containing the set of numbers
+    * @return nums - the updated array containing the same multiples of 10
     * @author: E. Aroozoo
     */
-    public static public int[] tenRun(int[] nums) {
+    public static int[] tenRun(int[] nums) {
 
-     
+    //Variables
+    int i;
+    int intMultiple;
+
+    intMultiple = 1;
+
+      for(i = 0; i < nums.length; i++) {
+        if(nums[i] % 10 == 0) {
+          intMultiple = nums[i];
+        } else if(nums[i] % 10 != 0 && intMultiple != 1) {
+          nums[i] = intMultiple;
+        }
+      }
+    return nums;
+    }
+    /**
+    *Given two arrays of ints sorted in increasing order, outer and inner, return true if all of the numbers in inner appear in outer. The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact that both arrays are already in sorted order
+    *
+    * @param inner - the inner array
+    * @param outer - the outer array 
+    * @return true/false if numbers in inner array are in outer array
+    * @author: E. Aroozoo
+    */
+    public static boolean linearIn(int[] outer, int[] inner) {
+      
     }
 //end of utility file
 }
