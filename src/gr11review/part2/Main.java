cbd570be964cfceb71a942db36464e8a107be33c
Braeden03 <braedenhugh@gmail.com>
2020-12-03 23:41:15 +0000

    Methods 2

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,21 @@
 package gr11review.part2;
 
+import java.io.*;
+import gr11review.part2.Utility.*;
+
+public class Main{
+  public static void main(String[] args) throws IOException{
+
+   BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    int intTotal1;
+    String strSumNumString;
+
+    System.out.println("Type a string");
+    strSumNumString = keyboard.readLine();
+
+    intTotal1 = Utility.sumNumbers(strSumNumString);
+    System.out.println(intTotal1);
+
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,43 @@
 package gr11review.part2;
 
+import java.io.*;
+import java.util.*;
+
 public class Utility {
+
+  //Methods 2
+  //add up all the numbers in a string to get a sumNumbers
+  //get length of string so to run for loop for correct ammount of time
+  
+  public static int sumNumbers(String str){
     
+    int intStringLength = 0;
+    int intCount;
+    char charCharacter;
+    int intTotal = 0;
+    String strString = "";
+
+    intStringLength = str.length();
+
+    for(intCount=0; intCount<intStringLength; intCount++){
+      charCharacter = str.charAt(intCount);
+
+      if(Character.isDigit(charCharacter)){
+        strString = strString+charCharacter;
+
+      }else if(!strString.equals("")){
+        intTotal = intTotal+ Integer.parseInt(strString);
+        strString = "";
+      }
+    }
+    if(!strString.equals("")){
+      intTotal = intTotal+ Integer.parseInt(strString);
+    }
+    return intTotal;
+  }
+
+  public static String alphaWord(String filenametxt){
+
+
+  }
 }
