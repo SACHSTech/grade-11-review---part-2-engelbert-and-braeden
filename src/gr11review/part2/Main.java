cec61fa339a6eff75e9252e26e3efc0f433200ac
Braeden03 <braedenhugh@gmail.com>
2020-12-06 18:20:32 +0000

    ...

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -6,16 +6,9 @@ import gr11review.part2.Utility.*;
 public class Main{
   public static void main(String[] args) throws IOException{
 
-   BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-
-    int intTotal1;
-    String strSumNumString;
-
-    System.out.println("Type a string");
-    strSumNumString = keyboard.readLine();
-
-    intTotal1 = Utility.sumNumbers(strSumNumString);
-    System.out.println(intTotal1);
-
+    String strAlphabet = "";
+    
+    strAlphabet = Utility.alphaword(Alphaword.txt);
+    System.out.println(strAlphabet);
   }
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -8,7 +8,7 @@ public class Utility {
   //Methods 2
   //add up all the numbers in a string to get a sumNumbers
   //get length of string so to run for loop for correct ammount of time
-  
+
   public static int sumNumbers(String str){
     
     int intStringLength = 0;
@@ -37,7 +37,34 @@ public class Utility {
   }
 
   public static String alphaWord(String filenametxt){
+    
+    //access a file and determine which word is alphabetically first
 
+    String strLine = "";
+    String strFirst = "";
+    String strFinal = "";
+    int intCompare = 0;
 
+    BufferedReader File = new BufferedReader(new FileReader(filenametxt));
+
+    strLine = File.readLine();
+    strFinal = File.readLine();
+
+    if(strLine == null){
+      File.close();
+      return "";
+    }else if(strLine != null){
+      while(strLine != null){
+        intCompare = strFinal.compareToIgnoreCase(strLine);
+        if(intCompare > 0){
+          strLine = strFinal;
+        }
+      strLine = File.readLine();
+
+      }
+
+    }
+    File.close();
+    return strFinal;
   }
 }
